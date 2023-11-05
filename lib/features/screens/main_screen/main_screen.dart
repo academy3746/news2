34731146945f79ed80a92c41816b0400c51f100f
ijonghyeon <academy3746@gmail.com>
2023-11-05 21:22:19 +0900

    뒤로가기 Action Handler 추가

--- lib/features/screens/main_screen/main_screen.dart
+++ lib/features/screens/main_screen/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news2/features/widgets/back_action_button.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -10,8 +11,28 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  /// Exit Application with double touch on Android
+  late BackHandlerButton _backHandlerButton;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _backHandlerButton = BackHandlerButton(context: context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return WillPopScope(
+      onWillPop: _backHandlerButton.onWillPop,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+        ),
+        body: Container(),
+      ),
+    );
   }
 }
